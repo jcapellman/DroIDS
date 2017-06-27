5fc9b39364721483c96ba8ee477f6f1582ba1220
Jarred Capellman <jcapellman@hotmail.com>
2017-06-26 21:35:17 -0500

    Initial Commit

---
+++ src/DroIDS/MainActivity.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace DroidIDS
+{
+    [Activity(Label = "DroidIDS", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+        }
+    }
+}
